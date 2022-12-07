c3f2d0597677233fe41df7b57b5a4672631b206a
Gabriel Sitorus <sitorusgg@gmail.com>
2022-12-07 17:07:52 +0700

    created models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save, post_delete
+from django.utils.text import slugify
+from django.urls import reverse
 
+import uuid
 # Create your models here.
+
+# uploading user files to a specific directory
+def user_directory_path(insatmce, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255, verbose_name='Tag')
+    slug = models.SlugField(null=False, unique=True, default=uuid.uuid1)
+
+    class Meta:
+        verbose_name = 'Tag'
+        verbose_name_plural = 'Tags'
+
+    def get_absolute_url(self):
+        return reverse("tags", args=[self.slug])
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.slug)
+        return super().save(*args, **kwargs)
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    image = models.ImageField(upload_to=user_directory_path, verbose_name='Image', null=True)
+    caption = models.CharField(max_length=1000, verbose_name='Caption')
+    created_at = models.DateTimeField(auto_now_add=True)
+    tag = models.ManyToManyField(Tag, related_name='tags')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+
+    def get_absolute_url(self):
+        return reverse("post-details", args=[str(self.id)])
+
+    def __str__(self):
+        return self.caption
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE,related_name='follower')
+    following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+class Stream(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stream_following')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stream_user')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField()
+
+    def add_post(sender, instance, *args, **kwargs):
+        post = instance
+        user = post.user
+        followers = Follow.objects.all().filter(following=user)
+        for follower in followers:
+            stream = Stream(post=post, user=follower.follower, date=post.created_at, following=user)
+            stream.save()
+
+post_save.connect(Stream.add_post, sender=Post)
