e271a410fbc3c09becd2fa020d62caf34db6072f
Gabriel Sitorus <sitorusgg@gmail.com>
2022-12-12 15:47:23 +0700

    part 9: profile

--- userauth/models.py
+++ userauth/models.py
@@ -2,9 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 from post.models import Post
 # Create your models here.
+
+def user_directory_path(instance, filename):
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='Profile')
+    first_name = models.CharField(max_length=255, null=True, blank=True)
+    last_name = models.CharField(max_length=255, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    url = models.CharField(max_length=1000, null=True, blank=True)
+    profile_info = models.TextField(max_length=255, null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
     favorite = models.ManyToManyField(Post)
+    image = models.ImageField(upload_to=user_directory_path, blank=True, null=True, verbose_name='Profile Image')
 
     def __str__(self):
         return self.user.username
