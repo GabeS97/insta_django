c521018bbcad4ee102bd9b95da91c59940d4eb40
Gabriel Sitorus <sitorusgg@gmail.com>
2022-12-14 04:14:55 -0800

    follow done, make function

--- userauth/views.py
+++ userauth/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from django.urls import resolve
-from post.models import Post
+from post.models import Post, Follow
 from .models import Profile
 # Create your views here.
 def userProfile(request, username):
@@ -14,6 +14,11 @@ def userProfile(request, username):
     else:
         posts.favorite.all()
 
+    # Tracking profile stats
+    post_count = Post.objects.filter(user=user).count()
+    following_count = Follow.objects.filter(follower=user).count()
+    follower_count = Follow.objects.filter(following=user).count()
+
     # Pagination
     paginator = Paginator(posts, 3)
     page_number = request.GET.get('page')
@@ -22,7 +27,10 @@ def userProfile(request, username):
     context = {
         'posts_paginator': posts_paginator,
         'profile': profile,
-        'posts': posts
+        'posts': posts,
+        'post_count': post_count,
+        'following_count': following_count,
+        'follower_count': follower_count
     }
 
     return render(request, 'profile.html', context)
